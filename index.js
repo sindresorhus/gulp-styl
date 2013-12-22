c9d27cc9c04921aab7b6e0d1adcd3c84ea9bdbcc
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-12-22 18:30:44 +1100

    Rename files to .css

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 var es = require('event-stream');
 var styl = require('styl');
+var gutil = require('gulp-util');
 
 module.exports = function (options) {
 	return es.map(function (file, cb) {
 		file.contents = new Buffer(styl(file.contents.toString(), options).toString());
+		file.path = gutil.replaceExtension(file.path, '.css');
 		cb(null, file);
 	});
 };
--- test.js
+++ test.js
@@ -6,10 +6,17 @@ var styl = require('./index');
 it('should preprocess CSS using Styl', function (cb) {
 	var stream = styl({compress: true});
 
-	stream.on('data', function (data) {
-		assert.equal(String(data.contents), '#test{width:50px;height:50px;}');
+	stream.on('data', function (newFile) {
+		assert.equal(newFile.path, '/home/gulp-styl/test/file.css');
+		assert.equal(newFile.relative, 'file.css');
+		assert.equal(String(newFile.contents), '#test{width:50px;height:50px;}');
 		cb();
 	});
 
-	stream.write(new gutil.File({contents: '#test{width:50px;height:@width;}'}));
+	stream.write(new gutil.File({
+		path: '/home/gulp-styl/test/file.styl',
+		base: '/home/gulp-styl/test/',
+		cwd: '/home/styl/',
+		contents: '#test{width:50px;height:@width;}'
+	}));
 });
