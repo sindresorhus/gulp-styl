07b54011f08f66562bda8e40cab564f6339aa64e
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-19 18:48:38 +0100

    switch to through2

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 'use strict';
-var es = require('event-stream');
-var styl = require('styl');
 var gutil = require('gulp-util');
+var through = require('through2');
+var styl = require('styl');
 
 module.exports = function (options) {
-	return es.map(function (file, cb) {
-		file.contents = new Buffer(styl(file.contents.toString(), options).toString());
-		file.path = gutil.replaceExtension(file.path, '.css');
-		cb(null, file);
+	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			this.push(file);
+			return cb();
+		}
+
+		if (file.isStream()) {
+			this.emit('error', new gutil.PluginError('gulp-styl', 'Streaming not supported'));
+			return cb();
+		}
+
+		try {
+			file.contents = new Buffer(styl(file.contents.toString(), options).toString());
+			file.path = gutil.replaceExtension(file.path, '.css');
+		} catch (err) {
+			this.emit('error', new gutil.PluginError('gulp-styl', err));
+		}
+
+		this.push(file);
+		cb();
 	});
 };
