67c168c9c9b730050d48cd08cd6af715ecaaff5c
Kevin Martensson <kevinmartensson@gmail.com>
2014-02-10 08:25:37 +0100

    Add support for plugins

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
+var _ = require('lodash');
 var gutil = require('gulp-util');
+var lastIsObject = _.compose(_.isPlainObject, _.last);
 var through = require('through2');
 var styl = require('styl');
 
-module.exports = function (options) {
+module.exports = function () {
+	var args = [].slice.call(arguments);
+	var options = lastIsObject(args) ? args.pop() : {};
+	var plugins = args;
+
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
 			this.push(file);
@@ -16,7 +22,9 @@ module.exports = function (options) {
 		}
 
 		try {
-			file.contents = new Buffer(styl(file.contents.toString(), options).toString());
+			var ret = styl(file.contents.toString());
+			plugins.forEach(ret.use.bind(ret));
+			file.contents = new Buffer(ret.toString(options));
 			file.path = gutil.replaceExtension(file.path, '.css');
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-styl', err));
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+/* global it */
 'use strict';
 var assert = require('assert');
+var calc = require('rework-calc');
 var gutil = require('gulp-util');
 var styl = require('./index');
 
@@ -17,5 +19,22 @@ it('should preprocess CSS using Styl', function (cb) {
 		base: '~/dev/gulp-styl/test',
 		path: '~/dev/gulp-styl/test/file.styl',
 		contents: new Buffer('#test{width:50px;height:@width;}')
+	}));
+});
+
+it('should support using plugins', function (cb) {
+	var stream = styl(calc);
+
+	stream.on('data', function (file) {
+		assert.equal(file.path, '~/dev/gulp-styl/test/file.css');
+		assert.equal(file.relative, 'file.css');
+		assert.equal(file.contents.toString(), '#test {\n  width: 50px;\n  height: 50px;\n}');
+		cb();
+	});
+
+	stream.write(new gutil.File({
+		base: '~/dev/gulp-styl/test',
+		path: '~/dev/gulp-styl/test/file.styl',
+		contents: new Buffer('#test{width:calc(25px + 25px);height:@width;}')
 	}));
 });
